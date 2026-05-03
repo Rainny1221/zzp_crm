53aaedc806b80f78ef640721286cc6f13994b396
Rainny <vu.luu@zzp.vn>
2026-05-03 11:19:37 +0700

    add: module

--- src/modules/crm-notifications/crm-notifications.module.ts
+++ src/modules/crm-notifications/crm-notifications.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { CrmNotificationsApplicationModule } from './application/crm-notifications.application.module';
 import { CrmNotificationsInfrastructureModule } from './infrastructure/crm-notifications.infrastructure.module';
 import { CrmNotificationsController } from './presentation/crm-notifications.controller';
 
 @Module({
   imports: [
+    CqrsModule,
     CrmNotificationsApplicationModule,
     CrmNotificationsInfrastructureModule,
   ],
   controllers: [CrmNotificationsController],
+  exports: [
+    CrmNotificationsApplicationModule,
+    CrmNotificationsInfrastructureModule,
+  ],
 })
 export class CrmNotificationsModule {}
